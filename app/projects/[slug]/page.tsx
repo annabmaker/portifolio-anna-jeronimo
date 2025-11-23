3f7b07937bd34a6ac6f1a705059b3082478039c5
annabmaker <annabmaker@gmail.com>
2025-11-23 01:51:55 -0300

    add rotas dinamicas no Portifolio

---
+++ app/projects/[slug]/page.tsx
@@ -0,0 +1,15 @@
+// [slug] indicam uma rota dinâmica ou seja, uma página que muda com base no conteúdo da URL. É uma forma de criar páginas personalizadas para cada item, como projetos, posts ou produtos.
+
+
+
+
+import { ProjectDetails } from "@/app/components/pages/project-datils";
+
+export default function Project () {
+    return(
+        <>
+        <ProjectDetails />
+        </>
+    )
+}
+
