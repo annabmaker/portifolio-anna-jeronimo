f88e4348199b22742d1ef6598bbc1e9f0b05be09
annabamker <annabmaker@gmail.com>
2025-11-23 14:48:35 -0300

    add querys com hygraphy

--- app/page.tsx
+++ app/page.tsx
@@ -5,9 +5,44 @@ import { KnownTechs } from "./components/pages/home/known-techs"
 import { Highlightedprojects } from "./components/pages/home/highlighted-project"
 import { WorkExperience } from "./components/pages/home/work-experience"
 import { BackToTop } from "./components/back-to-top"
+import { fetchHygraphQuery } from "./utils/fetch-hygraph-query"
+
+const getPageData = async () =>{
+   const query = `
+    query PageInfoQuery {
+      page(where: {slug: "home"}) {
+        introduction {
+          raw
+        }
+        technologies {
+          name
+        }
+        profilePicture {
+          url
+        }
+        socials {
+          url
+          iconSvg
+        }
+        knownTechs {
+          iconSvg
+          name
+          startDate
+        }
+      }
+    }
+  `
+  return fetchHygraphQuery(
+    query
+  )
+}
 
 
 export default async function Home() {
+  const response = await getPageData();
+
+  console.log(response)
+
   return (
     <>
     <BackToTop />
